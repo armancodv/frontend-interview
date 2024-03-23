a0c4704cdb10afc63c6dc3a25a6bcba7d60a0951
Arman Kolahan <akolahan@gmail.com>
2024-03-23 12:27:25 +0100

    build: eslint and prettier config

--- navigation/sidebar.ts
+++ navigation/sidebar.ts
@@ -1,25 +1,25 @@
-import {DefaultTheme} from "vitepress/types/default-theme";
+import { DefaultTheme } from 'vitepress/types/default-theme'
 
 export const sidebar: DefaultTheme.Sidebar = [
-    {
-        text: 'Javascript',
-        items: [
-            {text: 'Markdown Examples', link: '/markdown-examples'},
-            {text: 'Runtime API Examples', link: '/api-examples'}
-        ]
-    },
-    {
-        text: 'Typescript',
-        items: [
-            {text: 'Markdown Examples', link: '/markdown-examples'},
-            {text: 'Runtime API Examples', link: '/api-examples'}
-        ]
-    },
-    {
-        text: 'React',
-        items: [
-            {text: 'Markdown Examples', link: '/markdown-examples'},
-            {text: 'Runtime API Examples', link: '/api-examples'}
-        ]
-    }
+	{
+		text: 'Javascript',
+		items: [
+			{ text: 'Markdown Examples', link: '/markdown-examples' },
+			{ text: 'Runtime API Examples', link: '/api-examples' },
+		],
+	},
+	{
+		text: 'Typescript',
+		items: [
+			{ text: 'Markdown Examples', link: '/markdown-examples' },
+			{ text: 'Runtime API Examples', link: '/api-examples' },
+		],
+	},
+	{
+		text: 'React',
+		items: [
+			{ text: 'Markdown Examples', link: '/markdown-examples' },
+			{ text: 'Runtime API Examples', link: '/api-examples' },
+		],
+	},
 ]
--- navigation/social-links.ts
+++ navigation/social-links.ts
@@ -1,5 +1,5 @@
-import {DefaultTheme} from "vitepress/types/default-theme";
+import { DefaultTheme } from 'vitepress/types/default-theme'
 
 export const socialLinks: DefaultTheme.SocialLink[] = [
-    { icon: 'github', link: 'https://github.com/armancodv/frontend-interview' }
+	{ icon: 'github', link: 'https://github.com/armancodv/frontend-interview' },
 ]
